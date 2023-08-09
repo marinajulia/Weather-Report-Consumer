12fd9ee5906d00f289e65e4670428bbdd89d146c
marinajulia <marinajulia707@gmail.com>
2023-08-09 14:42:47 -0300

    Alter gitignore

--- WeatherReport.Domain/Service/WeatherReport/IWeatherReportService.cs
+++
@@ -1,9 +0,0 @@
-﻿using WeatherReport.Domain.Service.WeatherReport.Entities;
-
-namespace WeatherReport.Domain.Service.WeatherReport
-{
-    public interface IWeatherReportService
-    {
-        Task<WeatherReportEntity> GetWeatherReport(string cityId);
-    }
-}
