0c5ac3008b4a22ad8455d615f2f0922ea23e7393
marinajulia <marinajulia707@gmail.com>
2023-08-09 10:54:50 -0300

    first commit

---
+++ WeatherReport.Domain/Service/WeatherReport/IWeatherReportService.cs
@@ -0,0 +1,9 @@
+﻿using WeatherReport.Domain.Service.WeatherReport.Entities;
+
+namespace WeatherReport.Domain.Service.WeatherReport
+{
+    public interface IWeatherReportService
+    {
+        Task<WeatherReportEntity> GetWeatherReport(string cityId);
+    }
+}
---
+++ WeatherReport.Infra/Repositories/WeatherReport/WeatherReportRepository.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using WeatherReport.Domain.Service.WeatherReport;
+using WeatherReport.Domain.Service.WeatherReport.Entities;
+
+namespace WeatherReport.Infra.Repositories.WeatherReport
+{
+    public class WeatherReportRepository : IWeatherReportRepository
+    {
+        public async Task<WeatherReportEntity> GetWeatherReport(string cityId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://brasilapi.com.br/api/cptec/v1/clima/previsao/{cityId}");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                WeatherReportEntity? weatherReport = JsonConvert.DeserializeObject<WeatherReportEntity>(responseBody);
+
+                return weatherReport;
+            }
+        }
+    }
+}
